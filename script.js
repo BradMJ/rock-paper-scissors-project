a7ceb57841d411d47f935af29f6285b7ce540f37
BradMJ <90422753+BradMJ@users.noreply.github.com>
2022-09-12 10:38:13 -0700

    add function for a single rps round

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 
 let playerSelection;
 let computerSelection;
+let rock = 1;
+let paper = 2;
+let scissors = 3;
 
 function getComputerChoice() {
     let choice = Math.random() * 3;
@@ -17,6 +20,20 @@ function getComputerChoice() {
     return(choice);
 }
 
-function playRound(playerSelection, computerSelection) {
+//function playRound(playerSelection, computerSelection) {
+//    if (playerSelection == rock && computerSelection <= 1 || playerSelection == paper && computerSelection <= 2 || playerSelection == scissors && computerSelection <= 3) {
+//        return("Draw! Try again!");
+//    } else if (playerSelection > computerSelection) {
+//        return("Draw! try again!");
+//    } else 
+//}
 
-}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return("Draw! Try again!");
+    } else if (playerSelection > computerSelection) {
+        return(`You win! ${playerSelection} beats ${computerSelection}.`);
+    } else {
+        return(`You lose! ${computerSelection} beats ${playerSelection}.`);
+    }
+    }
