627e3e47a43f77562752f384426d40c2865903f6
BradMJ <90422753+BradMJ@users.noreply.github.com>
2022-09-27 11:55:58 -0700

    comment out first bad js inputs and create css file and link to html

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
+//let playerSelection;
+//let computerSelection;
+//let rock = 1;
+//let paper = 2;
+//let scissors = 3;
+
 //function getComputerChoice() {
-//    return( "Rock" || "Paper" || "Scissors" ) ;
+//    let choice = Math.random() * 3;
+//    if (choice <= 1) {
+//        choice = "Rock";
+//    } else if (choice <= 2) {
+//        choice = "Paper";
+//    } else {
+//        choice = "Scissors";
+//    }
+//    return(choice);
 //}
-
-let playerSelection;
-let computerSelection;
-let rock = 1;
-let paper = 2;
-let scissors = 3;
-
-function getComputerChoice() {
-    let choice = Math.random() * 3;
-    if (choice <= 1) {
-        choice = "Rock";
-    } else if (choice <= 2) {
-        choice = "Paper";
-    } else {
-        choice = "Scissors";
-    }
-    return(choice);
-}
 
 //function playRound(playerSelection, computerSelection) {
 //    if (playerSelection == rock && computerSelection <= 1 || playerSelection == paper && computerSelection <= 2 || playerSelection == scissors && computerSelection <= 3) {
@@ -28,12 +24,12 @@ function getComputerChoice() {
 //    } else 
 //}
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return("Draw! Try again!");
-    } else if (playerSelection > computerSelection) {
-        return(`You win! ${playerSelection} beats ${computerSelection}.`);
-    } else {
-        return(`You lose! ${computerSelection} beats ${playerSelection}.`);
-    }
-    }
+//function playRound(playerSelection, computerSelection) {
+//    if (playerSelection === computerSelection) {
+//        return("Draw! Try again!");
+//    } else if (playerSelection > computerSelection) {
+//        return(`You win! ${playerSelection} beats ${computerSelection}.`);
+//    } else {
+//        return(`You lose! ${computerSelection} beats ${playerSelection}.`);
+//    }
+//    }
