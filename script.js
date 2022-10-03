769f67b2224f2bfee1e49044cbf5652f0f388ed1
BradMJ <90422753+BradMJ@users.noreply.github.com>
2022-10-03 09:53:12 -0700

    complete rps game in console

--- script.js
+++ script.js
@@ -35,10 +35,72 @@
 //    }
 
 
-let choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let compScore = 0;
+
+let choices = ['rock', 'paper', 'scissors'];
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    logRounds();
+}
+
+function playRound() {
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+    checkWinner(playerSelection, computerSelection);
+}
+
+function getPlayerChoice() {
+    let input = prompt("Type rock, paper, or scissors.");
+    while (input == null) {
+        input = prompt("Type rock, paper, or scissors.");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while (check == false) {
+        input = prompt("Type rock, paper, or scissors. Nees correct spelling but not capitalization.");
+        while (input == null) {
+            input = prompt("Type rock, paper, or scissors.");
+        }
+        input = input.toLowerCase();
+        check = validateInput(input);
+    }
+    return input;
+}
 
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerChoice);
 }
 
+function validateInput(choice) {
+    return choices.includes(choice);
+}
+
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log(`Tie! You both picked ${playerSelection}.`);
+    } else if (
+    (playerSelection == 'rock' && computerSelection == 'scissors') ||
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')
+    ) {
+        playerScore++;
+        console.log("Player wins!");
+    } else {
+        compScore++;
+        console.log("Computer wins!");
+    }
+}
+
+function logRounds() {
+    if (playerScore > compScore) {
+        console.log("You beat the computer in a best of 5!");
+    } else if (playerScore < compScore) {
+        console.log("The computer beat you in a best of 5!");
+    } else {
+        console.log("You tied with the computer in a best of 5!");
+    }
+}
