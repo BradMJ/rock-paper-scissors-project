e8934136a9beb14c3c68a41e27a6264a409a8a28
BradMJ <90422753+BradMJ@users.noreply.github.com>
2023-01-30 14:04:01 -0800

    add ui edit date to readme, add buttons for rps in html, remove commented out old code in js and begin ui selectors and events, add basic styles for rps button boxes in css

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors-project
+
+Started adding UI 1/30/23
--- script.js
+++ script.js
@@ -1,51 +1,18 @@
-//let playerSelection;
-//let computerSelection;
-//let rock = 1;
-//let paper = 2;
-//let scissors = 3;
-
-//function getComputerChoice() {
-//    let choice = Math.random() * 3;
-//    if (choice <= 1) {
-//        choice = "Rock";
-//    } else if (choice <= 2) {
-//        choice = "Paper";
-//    } else {
-//        choice = "Scissors";
-//    }
-//    return(choice);
-//}
-
-//function playRound(playerSelection, computerSelection) {
-//    if (playerSelection == rock && computerSelection <= 1 || playerSelection == paper && computerSelection <= 2 || playerSelection == scissors && computerSelection <= 3) {
-//        return("Draw! Try again!");
-//    } else if (playerSelection > computerSelection) {
-//        return("Draw! try again!");
-//    } else 
-//}
-
-//function playRound(playerSelection, computerSelection) {
-//    if (playerSelection === computerSelection) {
-//        return("Draw! Try again!");
-//    } else if (playerSelection > computerSelection) {
-//        return(`You win! ${playerSelection} beats ${computerSelection}.`);
-//    } else {
-//        return(`You lose! ${computerSelection} beats ${playerSelection}.`);
-//    }
-//    }
-
-
 let playerScore = 0;
 let compScore = 0;
 
 let choices = ['rock', 'paper', 'scissors'];
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    logRounds();
-}
+// 1/30/23 first step, remove the logic that plays exactly 5 rounds
+// function game() {
+//    for (let i = 0; i < 5; i++) {
+//        playRound();
+//    }
+//    logRounds();
+//}
+// 1/30/23 second step, add event listeners to the three buttons that call playRound with
+// the correct playerSelection every time a button is clicked (keep console.log for this step)
+let selectRock = document.getElementById('rockButton');
 
 function playRound() {
     const playerSelection = getPlayerChoice();
